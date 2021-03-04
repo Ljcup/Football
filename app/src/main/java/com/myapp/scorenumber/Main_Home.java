3ba5492597acfe20d0cddd40f5406bfad40082c9
Janesh <janeshfultariya9>
2021-03-04 15:50:51 +0530

    V1

--- app/src/main/java/com/myapp/scorenumber/Main_Home.java
+++ app/src/main/java/com/myapp/scorenumber/Main_Home.java
@@ -1,19 +1,28 @@
 package com.myapp.scorenumber;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
 import androidx.appcompat.widget.Toolbar;
 
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
 
-public class Main_Home extends AppCompatActivity {
+public class Main_Home extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     DrawerLayout drawerLayout;
     NavigationView navigationView;
     Toolbar toolbar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,8 +37,50 @@ public class Main_Home extends AppCompatActivity {
 
         setSupportActionBar(toolbar);
 
+//
+//        Menu menu = navigationView.getMenu();
+//        menu.findItem(R.id.nav_logout).setVisible(true);
+//        menu.findItem(R.id.nav_profile).setVisible(true);
+
+        navigationView.bringToFront();;
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_closer);
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
+
+        navigationView.setNavigationItemSelectedListener(this);
+
+        navigationView.setCheckedItem(R.id.nav_match);
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)){
+
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+        else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+        switch (menuItem.getItemId()){
+            case R.id.nav_match:
+                break;
+            case R.id.nav_creatematch:
+                Intent intent = new Intent(Main_Home.this,Creatematch.class);
+                startActivity(intent);
+                break;
+            case R.id.nav_logout:
+                FirebaseAuth.getInstance().signOut();
+                break;
+        }
+
+        drawerLayout.closeDrawer(GravityCompat.START);
+
+        return true;
     }
 }
