f440a13346a32d392b8d4e24c23ad408fb590d54
Janesh <janeshfultariya9@gmail.com>
2021-03-31 12:04:35 +0530

    v1->n1 but n1->d1

--- app/src/main/java/com/myapp/scorenumber/Home.java
+++ app/src/main/java/com/myapp/scorenumber/Home.java
@@ -19,6 +19,7 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
